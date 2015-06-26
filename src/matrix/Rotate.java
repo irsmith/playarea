9306660cad22c2aa1babe4345799901ed2b6c0f3
Smith, Irene S. (ARC-TI)[SGT, INC] <irene.s.smith@nasa.gov>
2015-06-26 16:26:09 -0700

    print /roate matrix

--- src/matrix/Rotate.java
+++ src/matrix/Rotate.java
@@ -3,12 +3,10 @@ package matrix;
 public class Rotate {
 
 	public static void main(String[] args) {
-		int a[][] = new int[4][4];
-//		int a[][] = new int[3][4];
-//		int a[][] = new int[3][4];
-
+		// int a[][] = new int[4][4];
+		int a[][] = new int[3][3];
+		// int a[][] = new int[3][4];
 		// int a[][] = new int[][11]; Cannot specify an array dimension after an empty dim
-
 		// int a[][] = new int[1][]; //a is not null but points to null ie a[0] = null
 
 		printRoundabout(a);
@@ -26,29 +24,29 @@ public class Rotate {
 		int maxR = a.length;
 		int minC = 0;
 		int minR = 0;
-		int B=0;
-		for (int r=minR;r<maxR;r++) {
-			for (int c=minC;c<maxC;c++) {
-				 a[r][c] = B++;
+		int B = 0;
+		for (int r = minR; r < maxR; r++) {
+			for (int c = minC; c < maxC; c++) {
+				a[r][c] = B++;
 			}
 		}
 
 		while (true) {
 			// print top
 			for (int c = minC; c < maxC; c++) {
-				System.out.print(a[minR][c]+" ");
+				System.out.print(a[minR][c] + " ");
 			}
 			// print rside
-			for (int r = minR+1; r < maxR; r++) {
-				System.out.print(a[r][maxC - 1]+" ");
+			for (int r = minR + 1; r < maxR; r++) {
+				System.out.print(a[r][maxC - 1] + " ");
 			}
 			// print bottom
 			for (int c = maxC - 2; c > minC; c--) {
-				System.out.print(a[maxR-1][c]+" ");
+				System.out.print(a[maxR - 1][c] + " ");
 			}
 			// print leftside
 			for (int r = maxR - 1; r > minR; r--) {
-				System.out.print(a[r][minC]+" ");
+				System.out.print(a[r][minC] + " ");
 			}
 
 			maxC--;
@@ -56,7 +54,8 @@ public class Rotate {
 			minC++;
 			minR++;
 
-			if (minR<maxR && minC < maxC) break;
+			if (minR > maxR && minC > maxC)
+				break;
 		}
 	}
 
